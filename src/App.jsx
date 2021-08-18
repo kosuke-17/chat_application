15e344a3015a95ae71896cd4021177ee866887ae
kosuke-17 <tamutamu17@icloud.com>
2021-08-18 17:23:26 +0900

    ✨ Messageフォームの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <>test</>
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+
+import './App.css';
+
+const App =  () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="699b831c-515a-4e95-acae-b33927095690"
+      userName="Tamura Kosuke"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  )
 }
+export default App
